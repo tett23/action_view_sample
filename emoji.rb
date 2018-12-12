7717937e9ae0069503cb7dc5a329b1439c1950c1
tett23 <tett23@gmail.com>
2018-12-12 15:33:27 +0900

    Implement Emoji handler

--- emoji.rb
+++ emoji.rb
@@ -1,17 +1,22 @@
 require 'action_view'
+require 'somemoji'
 
 module ActionView
   module Template::Handlers
     class Emoji
       def call(template)
-        "'🎉'"
+        converted = Somemoji.emoji_one_emoji_collection.replace_code(template.source) do |emoji|
+          emoji.character
+        end
+
+        "'#{converted}'.to_s"
       end
     end
   end
 end
 EmojiHandler = ActionView::Template::Handlers::Emoji.new
 
-view = ActionView::Template.new('', 'partial', EmojiHandler, locals: [])
+view = ActionView::Template.new(':+1::tada::cyclone:', 'partial', EmojiHandler, {})
 
 class Context
 end
